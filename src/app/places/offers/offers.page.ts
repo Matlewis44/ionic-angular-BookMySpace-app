0a0a99c25952910a6b50e639d0c894197009374f
Mathias Engambé <maengambe@gmail.com>
2023-02-27 22:16:26 +0100

    27 Février

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { IonItemSliding, MenuController } from '@ionic/angular';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
@@ -14,10 +15,14 @@ et les mettre dans le tab d'offres
 export class OffersPage implements OnInit {
   offers: Place[] = [];
 
-  constructor(private placesService: PlacesService, private menuCtrl: MenuController) { }
+  constructor(private placesService: PlacesService, private router : Router, private menuCtrl: MenuController) { }
 
   ngOnInit() {
     this.offers = this.placesService.clonePlaces;
   }
 
+  onEdit(offerID: string | undefined, slidingItem: IonItemSliding){
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerID])
+  }
 }
